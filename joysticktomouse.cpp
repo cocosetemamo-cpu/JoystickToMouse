eadb6b2408fd56fc61b8a11af164a96dcb3c759e
cocosetemamo-cpu <cocosetemamo@gmail.com>
2026-08-19 20:38:23 -0300

    Create joysticktomouse.cpp

---
+++ joysticktomouse.cpp
@@ -0,0 +1,14 @@
+#include <windows.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "========================================\n";
+    std::cout << "          JoystickToMouse\n";
+    std::cout << "========================================\n\n";
+
+    std::cout << "Programa iniciado.\n";
+    std::cout << "Preparando controlador del joystick...\n\n";
+
+    return 0;
+}
